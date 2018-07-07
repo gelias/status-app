fb3462b775ae47321728c1b29818d1e8d36b2c12
Guilherme Elias <guilherme.elias@gmail.com>
2018-07-07 11:12:10 -0300

    Agora eh topi.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var version = { version: '1.0.0', status: 'OK', name: 'topi-status-real' }
+  var version = { version: '1.0.0', status: 'OK', name: 'topi' }
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(version));
 });
